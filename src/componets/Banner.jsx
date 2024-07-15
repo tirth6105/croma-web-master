5986eeb0834461612390b0023dd130ce971f9888
tirth kalathiya <tirthkalathiya6105@gmail.com>
2024-07-15 13:53:20 +0530

    project

--- src/componets/Banner.jsx
+++ src/componets/Banner.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
 import Carousel from 'react-bootstrap/Carousel';
-import b1 from '../assest/b1.jpg'
-import b2 from '../assest/b2.jpg'
-import b3 from '../assest/b3.jpg'
-import b4 from '../assest/b4.jpg'
+// import b1 from '../assest/b1.jpg'
+// import b2 from '../assest/b2.jpg'
+// import b3 from '../assest/b3.jpg'
+// import b4 from '../assest/b4.jpg'
 const Banner = () => {
     return (
         <>
             <div className="bg-black py-5 ">
                 <Carousel className='carousel slide'>
                     <Carousel.Item>
-                        <img src={b1} alt="" />
+                        <img src='../assest/b1.jp' alt="" />
                     </Carousel.Item>
-                    <Carousel.Item>
+                    {/* <Carousel.Item>
                         <img src={b2} alt="" />
                     </Carousel.Item>
                     <Carousel.Item>
@@ -20,7 +20,7 @@ const Banner = () => {
                     </Carousel.Item>
                     <Carousel.Item>
                         <img src={b4} alt="" />
-                    </Carousel.Item>
+                    </Carousel.Item> */}
                 </Carousel>
             </div>
         </>
